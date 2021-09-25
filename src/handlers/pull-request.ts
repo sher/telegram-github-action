48d947886418d6716aba7b67493db33c04a055cd
sher <sherzod@gmail.com>
2021-09-25 22:06:19 +0900

    upd

--- src/handlers/pull-request.ts
+++ src/handlers/pull-request.ts
@@ -2,6 +2,8 @@ import * as core from '@actions/core';
 import axios from 'axios';
 import type { Context, PullRequestPayload } from '../../types';
 
+const BASE_URL = 'https://api.telegram.org/bot';
+
 export async function handlePullRequest(context: Context<PullRequestPayload>) {
   switch (context.payload.action) {
     case 'opened':
@@ -17,8 +19,12 @@ export async function handlePullRequest(context: Context<PullRequestPayload>) {
 async function handleOpenedAction(context: Context<PullRequestPayload>) {
   const TELEGRAM_BOT_TOKEN = core.getInput('TELEGRAM_BOT_TOKEN');
   const TELEGRAM_CHAT_ID = core.getInput('TELEGRAM_CHAT_ID');
+  const SEND_MESSAGE_URL = `${BASE_URL}${TELEGRAM_BOT_TOKEN}/sendMessage`;
 
-  console.log(context, TELEGRAM_CHAT_ID);
+  axios.post(SEND_MESSAGE_URL, {
+    chat_id: TELEGRAM_CHAT_ID,
+    text: context.payload.pull_request.title
+  });
 }
 
 async function handleUnsupportedAction(context: Context<PullRequestPayload>) {
